54847856c34a505cc6823ed4d9cfb77ba6547bd2
Raj Pandey <raj@Rajs-MacBook-Pro.local>
2021-06-08 16:50:37 -0400

    test

--- src/Hero.js
+++ src/Hero.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Taj from "./Taj.png";
+import chic from "./chic.jpg";
 
 function Hero() {
   return (
     <div className="hero">
       <img className="taj" src={Taj} alt="gro" />
       <p>Live the life with full of fun ~stefania</p>
+      <img className="chic" src={chic} alt="gro" />
     </div>
   );
 }
