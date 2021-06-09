5374acdfe88bc9d91cdd2b61d70e57520c094b71
Raj Pandey <raj@Rajs-MacBook-Pro.local>
2021-06-09 02:36:12 -0400

    social change

--- src/Hero.js
+++ src/Hero.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Taj from "./Taj.png";
-import chic from "./chic.jpg";
+import chic from "./chic.png";
 
 function Hero() {
   return (
     <div className="hero">
-      <img className="taj" src={Taj} alt="gro" />
-      <p>Live the life with full of fun ~stefania</p>
       <img className="chic" src={chic} alt="gro" />
+      <p>Live the life with full of fun ~stefania</p>
+      <img className="taj" src={Taj} alt="gro" />
     </div>
   );
 }
